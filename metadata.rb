cc805574a795d7a2f729cc5ef54169cb7d20bce8
jtimberman <joshua@opscode.com>
2013-08-20 10:46:13 -0600

    change maintainer

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             "snort"
-maintainer       "Opscode, Inc."
-maintainer_email "cookbooks@opscode.com"
+maintainer       "Joshua Timberman"
+maintainer_email "cookbooks@housepub.org"
 license          "Apache 2.0"
 description      "Installs/Configures snort"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.10.0"
+version          "0.10.1"
 
 recipe "snort", "Installs snort packages based on platform"
 
