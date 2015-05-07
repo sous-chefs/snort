ba12b6c1ae65ea37f871460037512eba6a232eeb
Tim Smith <tim@cozy.co>
2015-05-07 09:22:30 -0700

    Bump version for development and fix description

--- metadata.rb
+++ metadata.rb
@@ -2,9 +2,9 @@ name             'snort'
 maintainer       'Joshua Timberman'
 maintainer_email 'cookbooks@housepub.org'
 license          'Apache 2.0'
-description      'Installs/Configures snort'
+description      'Installs Snort IDS packages'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 recipe 'snort', 'Installs snort packages based on platform'
 
