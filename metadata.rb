46f8247d59417cf1374541814fd37f5478364889
Derek Ditch <derek@criticalstack.com>
2015-09-30 15:42:14 -0500

    Fixed path to dynamic libraries.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@housepub.org'
 license          'Apache 2.0'
 description      'Installs Snort IDS packages'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.4'
+version          '1.0.5'
 
 recipe 'snort', 'Installs snort packages based on platform'
 
