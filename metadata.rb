3b875912497466d1ade551314c8893463ae2c8d2
Tim Smith <tsmith84@gmail.com>
2015-10-09 21:45:20 -0700

    Add new supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -11,3 +11,6 @@ recipe 'snort', 'Installs snort packages based on platform'
 %w(ubuntu debian redhat centos fedora scientific amazon oracle).each do |os|
   supports os
 end
+
+source_url 'https://github.com/jtimberman/snort-cookbook' if respond_to?(:source_url)
+issues_url 'https://github.com/jtimberman/snort-cookbook/issues' if respond_to?(:issues_url)
