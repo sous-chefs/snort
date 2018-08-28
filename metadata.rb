7aac7b4dc42f310d6a299c7f27465d28c190dcf2
Dan Webb <dan.webb@damacus.io>
2018-08-28 09:45:15 +0100

    Release 3.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'help@sous-chefs.org'
 license          'Apache-2.0'
 description      'Installs Snort IDS packages'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '3.0.0'
-chef_version     '>= 12.7' if respond_to?(:chef_version)
+version          '3.0.1'
+chef_version     '>= 12.7'
 
 %w(ubuntu debian redhat centos fedora scientific amazon oracle).each do |os|
   supports os
