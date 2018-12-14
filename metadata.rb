7f8f18a30df6843872c1347875752b7eac69740f
Simon W. Gill <swgill@cyberrange.internal>
2018-12-14 15:43:42 +0000

    Updated checksums after version change of CentOS and Fedora RPMs on snort.org

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'help@sous-chefs.org'
 license          'Apache-2.0'
 description      'Installs Snort IDS packages'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '4.0.0'
+version          '4.0.1'
 chef_version     '>= 13.0'
 source_url       'https://github.com/sous-chefs/snort'
 issues_url       'https://github.com/sous-chefs/snort/issues'
